42ba5bcfb80aa9d6b0b050f636bd66081ac6659b
Alberto <albertofots@gmail.com>
2025-01-26 19:30:41 +0100

    simple configuration.nix file

---
+++ configuration.nix
@@ -0,0 +1,78 @@
+{ config, lib, pkgs, ... }
+
+{
+	imports = 
+	[ # Include the results of the hardware scan
+		./hardware-configuration.nix
+	];
+
+	# Use the systemd-boot EFI boot loader.
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+
+	# Networking
+	networking.hostname = "soxin";
+	networking.wireless.enable = true;
+	networking.networkmanager.enable = true;
+
+	# Set your time zone.
+	time.timeZone = "Europe/Rome";
+
+	# Select internationalization properties
+	i 18n.defaultLocale = "en_US.UTF-8";
+	console = {
+		font = "Lat2-Terminus16";
+	#        keyMap = "us";
+		useXkbConfig = true; # use xkb.options in tty
+	};
+
+	# Enable flakes
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+	# Enable the X11 windowing system
+	services.xserver.enable = true;
+
+	# Enable sound
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+		jack.enable = true;
+	};
+
+
+	# Enable display manager
+	services.displayManager.sddm.enable = true;
+
+	# Enable XFCE4 desktop environment
+	services.xserver.desktopManager.xfce.enable = true;
+	services.displayManager.defaultSession = "xfce";	
+
+	# Define a user account
+	users.users.arstneio = {
+		isNormalUser = true;
+		home = "/home/arstneio";
+		extraGroups = [ "wheel" ];
+		password = "bob"
+		packages = with pkgs; [
+			tree
+			brave
+		];
+	};
+
+	# List packages installed in system profile. To search, run:
+	# $ nix search wget
+	environment.systemPackages = with pkgs; [
+		git
+		curl
+		kitty
+		vim
+		neofetch
+	];
+	
+	# System state
+	#system.stateVersion = "24.11";
+}
+	
